73e29876401c64d08d26f37859a8cfeb7b00c859
Issayev Abzal <61589482+Karisbala@users.noreply.github.com>
2023-02-21 22:59:47 +0600

    Initial commit

---
+++ lab4/README.md
@@ -0,0 +1,20 @@
+# Online store
+
+## Show your understanding of Angular.
+
+1. Complete project from tutorial Your First App
+2. Implement component “Products”, which shows the list of products. Each product must have following properties:
+- image
+- name
+- description
+- rating
+3. Each product should be linked to a real item from Kaspi.kz
+4. Must be at least 10 products
+5. Buttons “Share” for each product, by clicking, user should be redirected to Whatsapp or Telegram with the link to the product 
+6. Optional: Add product image gallery, minimum 3 images.
+
+
+
+
+
+GOOD LUCK :)
---
+++ lab4/online-shop/src/app/products.ts
@@ -0,0 +1,118 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  rating: number;
+  url: string;
+  image_url: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Смартфон Apple iPhone 13 128Gb черный',
+    price: 373770,
+    description: 'A large phone with one of the best screens',
+    rating: 9.1,
+    url: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-chernyi-102298404/?c=750000000#!/item',
+    image_url:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h49/hc0/46392662523934/apple-iphone-13-128gb-cernyj-102298404-1-Container.jpg',
+  },
+  {
+    id: 2,
+    name: 'Смартфон Xiaomi Redmi 10C 4 ГБ/128 ГБ серый',
+    price: 71988,
+    description: 'A large phone with one of the best screens',
+    rating: 9.1,
+    url: 'https://kaspi.kz/shop/p/xiaomi-redmi-10c-4-gb-128-gb-seryi-104417231/?c=750000000#!/item',
+    image_url:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hbc/h0a/49939925205022/xiaomi-redmi-10c-4-gb-128-gb-seryj-104417231-1.jpg',
+  },
+  {
+    id: 3,
+    name: 'Смартфон Samsung Galaxy A13 4 ГБ/128 ГБ черный',
+    price: 90185,
+    description: 'A large phone with one of the best screens',
+    rating: 9.1,
+    url: 'https://kaspi.kz/shop/p/samsung-galaxy-a13-4-gb-128-gb-chernyi-104253279/?c=750000000#!/item',
+    image_url:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h90/h49/49613711966238/samsung-galaxy-a13-4-gb-128-gb-chernyi-104253279-1.jpg',
+  },
+  {
+    id: 4,
+    name: 'Смартфон Apple iPhone 11 128Gb Slim Box черный',
+    price: 293100,
+    description: 'A large phone with one of the best screens',
+    rating: 9.1,
+    url: 'https://kaspi.kz/shop/p/apple-iphone-11-128gb-slim-box-chernyi-100692388/?c=750000000#!/item',
+    image_url:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h17/h14/31489167425566/apple-iphone-11-128gb-slim-box-cernyj-100692388-1-Container.jpg',
+  },
+  {
+    id: 5,
+    name: 'Смартфон Xiaomi Redmi Note 10 Pro 8 ГБ/256 ГБ серый',
+    price: 128350,
+    description: 'A large phone with one of the best screens',
+    rating: 9.1,
+    url: 'https://kaspi.kz/shop/p/xiaomi-redmi-note-10-pro-8-gb-256-gb-seryi-107221005/?c=750000000#!/item',
+    image_url:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h45/h27/64487158087710/xiaomi-redmi-note-10-pro-8-gb-128-gb-seryi-107221005-1.jpg',
+  },
+  {
+    id: 6,
+    name: 'Смартфон Samsung Galaxy A23 6 ГБ/128 ГБ черный',
+    price: 104615,
+    description: 'A large phone with one of the best screens',
+    rating: 9.1,
+    url: 'https://kaspi.kz/shop/p/samsung-galaxy-a23-6-gb-128-gb-chernyi-104348541/?c=750000000#!/item',
+    image_url:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hb5/ha6/49792685244446/smartfon-samsung-galaxy-a23-sm-a235fzkkskz-128gb-black-104348541-1.jpg',
+  },
+  {
+    id: 7,
+    name: 'Смартфон Xiaomi Redmi Note 11 Pro 8 ГБ/128 ГБ серый',
+    price: 139180,
+    description: 'A large phone with one of the best screens',
+    rating: 9.1,
+    url: 'https://kaspi.kz/shop/p/xiaomi-redmi-note-11-pro-8-gb-128-gb-seryi-103971386/?c=750000000#!/item',
+    image_url:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h11/h23/51166990368798/xiaomi-redmi-note-11-pro-8-gb-128-gb-seryi-103971386-1.jpg',
+  },
+  {
+    id: 8,
+    name: 'Смартфон Apple iPhone 14 Pro Max 256Gb фиолетовый',
+    price: 703690,
+    description: 'A large phone with one of the best screens',
+    rating: 9.1,
+    url: 'https://kaspi.kz/shop/p/apple-iphone-14-pro-max-256gb-fioletovyi-106363342/?c=750000000#!/item',
+    image_url:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h00/h18/62948780834846/apple-iphone-14-pro-max-128gb-fioletovyj-106363342-1.jpg',
+  },
+  {
+    id: 9,
+    name: 'Смартфон Apple iPhone 14 128Gb черный',
+    price: 422990,
+    description: 'A large phone with one of the best screens',
+    rating: 9.1,
+    url: 'https://kaspi.kz/shop/p/apple-iphone-14-128gb-chernyi-106363023/?c=750000000#!/item',
+    image_url:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h8f/h61/63072540131358/apple-iphone-14-128gb-cernyj-106363023-1.jpg',
+  },
+  {
+    id: 10,
+    name: 'Смартфон Samsung Galaxy A33 5G 6 ГБ/128 ГБ черный',
+    price: 129690,
+    description: 'A large phone with one of the best screens',
+    rating: 9.1,
+    url: 'https://kaspi.kz/shop/p/samsung-galaxy-a33-5g-6-gb-128-gb-chernyi-104398547/?c=750000000#!/item',
+    image_url:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h56/h8c/49894748160030/samsung-galaxy-a33-5g-6-gb-128-gb-cernyj-104398547-1.jpg',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
